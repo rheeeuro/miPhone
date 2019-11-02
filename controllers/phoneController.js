b2c57e912c029ada5d4d337029129e1a55294e5d
eurohand <eurohand@naver.com>
2019-11-02 21:32:03 +0900

    Search Controller

--- controllers/phoneController.js
+++ controllers/phoneController.js
@@ -1,7 +1,20 @@
-export const home = (req, res) => res.render("Home");
-export const search = (req, res) => res.render("Search");
-export const phones = (req, res) => res.render("Phone");
-export const upload = (req, res) => res.render("Upload");
-export const phoneDetail = (req, res) => res.render("Phone Detail");
-export const editPhone = (req, res) => res.render("Edit Phone");
-export const deletePhone = (req, res) => res.render("Delete Phone");
+export const home = (req, res) => res.render("home", { pageTitle: "홈" });
+
+export const search = (req, res) => {
+  const {
+    query: { term: searchingBy }
+  } = req;
+  res.render("search", { pageTitle: "검색", searchingBy });
+};
+
+export const upload = (req, res) =>
+  res.render("upload", { pageTitle: "휴대폰 정보 등록" });
+
+export const phoneDetail = (req, res) =>
+  res.render("phoneDetail", { pageTitle: "휴대폰 정보" });
+
+export const editPhone = (req, res) =>
+  res.render("editPhone", { pageTitle: "휴대폰 정보 수정" });
+
+export const deletePhone = (req, res) =>
+  res.render("deletePhone", { pageTitle: "휴대폰 정보 삭제" });
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,15 @@
-export const join = (req, res) => res.render("join");
-export const login = (req, res) => res.render("login");
-export const logout = (req, res) => res.render("logout");
-export const userDetail = (req, res) => res.render("userDetail");
-export const editProfile = (req, res) => res.render("editProfile");
-export const changePassword = (req, res) => res.render("changePassword");
+export const join = (req, res) => res.render("join", { pageTitle: "회원가입" });
+
+export const login = (req, res) => res.render("login", { pageTitle: "로그인" });
+
+export const logout = (req, res) =>
+  res.render("logout", { pageTitle: "로그아웃" });
+
+export const userDetail = (req, res) =>
+  res.render("userDetail", { pageTitle: "사용자 정보" });
+
+export const editProfile = (req, res) =>
+  res.render("editProfile", { pageTitle: "프로필 편집" });
+
+export const changePassword = (req, res) =>
+  res.render("changePassword", { pageTitle: "비밀번호 변경" });
