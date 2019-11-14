f13a9b1ac1a5cab4c4a38979877b84f2f427eda5
eurohand <eurohand@naver.com>
2019-11-14 23:41:40 +0900

    MongoStore and Middlewares

--- routers/phoneRouter.js
+++ routers/phoneRouter.js
@@ -8,21 +8,22 @@ import {
   getEditPhone,
   postEditPhone
 } from "../controllers/phoneController";
+import { onlyPrivate } from "../middlewares";
 
 const phoneRouter = express.Router();
 
 // Upload
-phoneRouter.get(routes.upload, getUpload);
-phoneRouter.post(routes.upload, postUpload);
+phoneRouter.get(routes.upload, onlyPrivate, getUpload);
+phoneRouter.post(routes.upload, onlyPrivate, postUpload);
 
 // Phone Detail
 phoneRouter.get(routes.phoneDetail(), phoneDetail);
 
 // Edit Phone
-phoneRouter.get(routes.editPhone(), getEditPhone);
-phoneRouter.post(routes.editPhone(), postEditPhone);
+phoneRouter.get(routes.editPhone(), onlyPrivate, getEditPhone);
+phoneRouter.post(routes.editPhone(), onlyPrivate, postEditPhone);
 
 // Delete Phone
-phoneRouter.get(routes.deletePhone(), deletePhone);
+phoneRouter.get(routes.deletePhone(), onlyPrivate, deletePhone);
 
 export default phoneRouter;
