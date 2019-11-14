782ba3511d313c767cb6661d226a56798ed10f68
eurohand <eurohand@naver.com>
2019-11-14 23:33:58 +0900

    Sessions on Express

--- middlewares.js
+++ middlewares.js
@@ -3,6 +3,7 @@ import routes from "./routes";
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "miPhone";
   res.locals.routes = routes;
-  res.locals.user = req.user || {};
+  res.locals.user = req.user || null;
+  console.log(req.user);
   next();
 };
