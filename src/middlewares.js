4d3750d08009d4fcaee3dc5e812a137de2a8e1f3
eurohand <eurohand@naver.com>
2019-11-25 05:38:04 +0900

    Admin ID

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,5 +1,6 @@
-import routes from "./routes";
 import dotenv from "dotenv";
+import routes from "./routes";
+import User from "./models/User";
 
 dotenv.config();
 
@@ -24,11 +25,16 @@ export const onlyPrivate = (req, res, next) => {
   }
 };
 
+export const adminID = async () => {
+  const admin = await User.findById(process.env.adminID);
+  return admin;
+};
+
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "miPhone";
   res.locals.routes = routes;
   res.locals.loggedUser = req.user || null;
   res.locals.formatDate = formatDate;
-  res.locals.adminID = process.env.adminID;
+  res.locals.admin = adminID();
   next();
 };
